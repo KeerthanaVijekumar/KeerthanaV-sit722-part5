53e52aa8a1e01fbd8e6c7dbb8a3ca13ec77afa98
KeerthanaVijekumar <s224719679@deakin.edu.au>
2024-10-02 21:57:18 +1000

    fix for role assignment on index

--- scripts/terraform/kubernetes-cluster.tf
+++ scripts/terraform/kubernetes-cluster.tf
@@ -26,7 +26,7 @@ resource "azurerm_kubernetes_cluster" "cluster" {
 
 # Attaches the container registry to the cluster
 resource "azurerm_role_assignment" "role_assignment" {
-  principal_id                     = azurerm_kubernetes_cluster.cluster.kubelet_identity[0].object_id
+  principal_id                     = azurerm_kubernetes_cluster[0].cluster.kubelet_identity[0].object_id
   role_definition_name             = "AcrPull"
   scope                            = azurerm_container_registry.container_registry[0].id
   skip_service_principal_aad_check = true
