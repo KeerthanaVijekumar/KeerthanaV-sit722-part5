9ffe680447b40fd413bc04390e8d3535c4233aa7
KeerthanaVijekumar <s224719679@deakin.edu.au>
2024-10-02 21:51:23 +1000

    fix for index resource

--- scripts/terraform/container-registry.tf
+++ scripts/terraform/container-registry.tf
@@ -1,14 +1,14 @@
 # Check if the container registry exists
 data "azurerm_container_registry" "existing_acr" {
   name                = var.app_name
-  resource_group_name = azurerm_resource_group.sit722part5.name
+  resource_group_name = azurerm_resource_group.sit722part5[0].name
 }
 
 # Create the container registry only if it does not exist
 resource "azurerm_container_registry" "container_registry" {
   count               = length(data.azurerm_container_registry.existing_acr.id) == 0 ? 1 : 0
   name                = var.app_name
-  resource_group_name = azurerm_resource_group.sit722part5.name
+  resource_group_name = azurerm_resource_group.sit722part5[0].name
   location            = var.location
   admin_enabled       = true
   sku                 = "Basic"
