586401c3e9a0c272a45680c7edacc1ad072b1a3b
KeerthanaVijekumar <s224719679@deakin.edu.au>
2024-10-02 21:53:39 +1000

    fix for role assignment

--- scripts/terraform/kubernetes-cluster.tf
+++ scripts/terraform/kubernetes-cluster.tf
@@ -28,6 +28,6 @@ resource "azurerm_kubernetes_cluster" "cluster" {
 resource "azurerm_role_assignment" "role_assignment" {
   principal_id                     = azurerm_kubernetes_cluster.cluster.kubelet_identity[0].object_id
   role_definition_name             = "AcrPull"
-  scope                            = azurerm_container_registry.container_registry.id
+  scope                            = azurerm_container_registry.container_registry[0].id
   skip_service_principal_aad_check = true
 }
